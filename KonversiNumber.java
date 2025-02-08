6cb5909110d9eef719854faf9b12b04a00c795db
Yuliana Hapsari <yuliahapz@gmail.com>
2025-02-08 22:24:07 +0700

    Day 1

---
+++ KonversiNumber.java
@@ -0,0 +1,12 @@
+public class KonversiNumber {
+    public static void main(String[] args) {
+        byte iniByte = 10;
+        short iniShort = iniByte;
+        int iniInt = iniShort;
+        long iniLong = iniInt;
+        float iniFloat = iniLong;
+        double iniDouble = iniFloat;
+
+        float iniFloat2 = (float) iniDouble;
+    }
+}
---
+++ TipeDataNumber.java
@@ -0,0 +1,20 @@
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 10000;
+        long iniLong = 1000000;
+        long iniLong2 = 1000000L;
+
+        float iniFloat = 10.10f;
+        double iniDouble = 10.10;
+
+        int decimalInt = 25;
+        int hexInt = 0xA132B;
+        int binInt = 0b01010101;
+
+        long balance = 1_000_000_000;
+        int sum = 60_000_000;
+
+    }
+}
